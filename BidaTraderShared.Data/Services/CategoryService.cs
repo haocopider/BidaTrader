bb92e4266f26d0d158d7ceaa061175f4fcf1d05e
haocopider <hao.jukain.dev.game@gmail.com>
2025-12-05 09:27:33 +0700

    Update Data/Services

---
+++ BidaTraderShared.Data/Services/CategoryService.cs
@@ -0,0 +1,42 @@
+﻿using BidaTraderShared.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text;
+
+namespace BidaTraderShared.Data.Services
+{
+    public class CategoryService : IService<Category>
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoryService(HttpClient httpClient) => _httpClient = httpClient;
+        public async Task<bool> CreateItemAsync(Category item)
+        {
+            var reponse = await _httpClient.PostAsJsonAsync($"api/categories", item);
+            return reponse.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteItemAsync(int id)
+        {
+            var reponse = await _httpClient.DeleteAsync($"api/categories/{id}");
+            return reponse.IsSuccessStatusCode;
+        }
+
+        public async Task<Category?> GetItemByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Category>($"api/categories/{id}");
+        }
+
+        public async Task<List<Category>?> GetItemsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Category>>("api/categories");
+        }
+
+        public async Task<bool> UpdateItemAsync(Category item)
+        {
+            var reponse = await _httpClient.PutAsJsonAsync($"api/categories/{item.Id}", item);
+            return reponse.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ BidaTraderShared.Data/Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using BidaTraderShared.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text;
+
+namespace BidaTraderShared.Data.Services
+{
+    public class ProductService : IService<Product>
+    {
+        private readonly HttpClient _httpClient;
+        public ProductService(HttpClient httpClient) => _httpClient = httpClient;
+        public async Task<bool> CreateItemAsync(Product item)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/products", item);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteItemAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/products/{id}");
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<Product?> GetItemByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Product>($"api/products/{id}");
+        }
+
+        public async Task<List<Product>?> GetItemsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Product>>("api/products");
+        }
+
+        public async Task<bool> UpdateItemAsync(Product item)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/products/{item.Id}", item);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
